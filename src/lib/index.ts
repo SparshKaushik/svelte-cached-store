4b30b5bed85e6291d2a4995e79382b22a363f2fa
Sparsh Kaushik <contact@sparshkaushik.com>
2023-04-15 17:14:16 +0530

    Initial Commit - Basic BareBone of the Library

---
+++ src/lib/index.ts
@@ -0,0 +1,29 @@
+import { browser } from "$app/environment";
+import { writable } from "svelte/store";
+
+export function cachedwritable<type>(value: type, id: string) {
+    if (browser && window.localStorage.getItem(id) != null) {
+        switch (typeof value) {
+            case "number":
+                // @ts-ignore
+                value = Number(window.localStorage.getItem(id)!)
+                break;
+            case "boolean":
+                // @ts-ignore
+                value = Boolean(window.localStorage.getItem(id)!)
+                break;
+            case "string":
+                // @ts-ignore
+                value = String(window.localStorage.getItem(id)!)
+                break;
+            case "object":
+                value = JSON.parse(window.localStorage.getItem(id)!)
+                break;
+        }
+    }
+    const newStore = writable<type>(value)
+    newStore.subscribe((value) => {
+        browser && window.localStorage.setItem(id, String(value))
+    })
+    return newStore
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { cachedwritable } from "$lib";
+
+    const testStore = cachedwritable<string>("bruh", "testStore")
+
+    setTimeout(()=>{
+        testStore.set("ok")
+    }, 1000)
+</script>
+
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
