1cd40c61dae09e2398b19b55ffcc192fe82750aa
Sparsh Kaushik <contact@sparshkaushik.com>
2023-04-25 10:22:27 +0530

    Another attempt to fix parsing

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,23 +3,7 @@ import { writable } from "svelte/store";
 
 export function cachedwritable<type>(value: type, id: string) {
     if (browser && window.localStorage.getItem(id) != null) {
-        switch (typeof value) {
-            case "number":
-                // @ts-ignore
-                value = Number(window.localStorage.getItem(id)!)
-                break;
-            case "boolean":
-                // @ts-ignore
-                value = window.localStorage.getItem(id)
-                break;
-            case "string":
-                // @ts-ignore
-                value = String(window.localStorage.getItem(id)!)
-                break;
-            default:
-                value = JSON.parse(window.localStorage.getItem(id)!)
-                break;
-        }
+        JSON.parse(window.localStorage.getItem(id)!)
     }
     const newStore = writable<type>(value)
     newStore.subscribe((value) => {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import { cachedwritable } from "$lib";
 
-    const testStore = cachedwritable<boolean>(false, "testStore")
+    const testStore = cachedwritable<number>(99, "testStore")
 
     setTimeout(()=>{
-        testStore.set(false)
+        testStore.set($testStore + 1)
     }, 1000)
 
     testStore.subscribe((value) => {
-        // console.log(value)
+        console.log(value)
     })
 </script>
 
